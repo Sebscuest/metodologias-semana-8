c89737bc1e8ad2e1f25321e615869ea09b8f7073
Sebscuest <kscuesta@sudamericano.edu.ec>
2024-07-02 09:47:20 -0500

    Update array.js

--- deberMom3-MetodoArray/ejer1/array.js
+++ deberMom3-MetodoArray/ejer1/array.js
@@ -4,11 +4,14 @@
 nums = [13.676767, 3.123432, 5.34543543, 10.3452345]*/
 
 function filRedondeo(nums) {
-    return nums
-      .filter(num => num > 10)
-      .map(num => Number(num.toFixed(2)).toString());
+    let resultado = [];
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] > 10) {
+        resultado.push(Number(nums[i].toFixed(2)).toString());
+      }
+    }
+    return resultado;
   }
   
   const nums = [13.676767, 3.123432, 5.34543543, 10.3452345];
-  alert(filRedondeo
-(nums));
+  alert(filRedondeo(nums));
